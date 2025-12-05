a5633d1b1a97584fb91a6064ac0ab30440361e99
aditya <adityanj7800@gmail.com>
2025-12-05 22:44:26 +0530

    5 dec:-textfield,padding,container,border,keyboardtype,textbutton

--- currency_converter/lib/currency_converter_materialapp.dart
+++ currency_converter/lib/currency_converter_materialapp.dart
@@ -1,10 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialpage extends StatelessWidget {
   const CurrencyConverterMaterialpage({super.key});
   @override
+  //build function use only to write simple variable and function not like timer or complex code
   Widget build(BuildContext context) {
-    return const Scaffold(
+    //created a border funtion
+    final border = const OutlineInputBorder(
+      //Border Work
+      borderSide: const BorderSide(
+        color: Colors.lime,
+        width: 1.0,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignOutside,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    );
+    return Scaffold(
       //for property of Scaffold
       backgroundColor: Color.fromARGB(255, 111, 157, 180),
       // body: ColoredBox(
@@ -19,32 +32,75 @@ class CurrencyConverterMaterialpage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '1',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 187, 84, 76),
+            Container(
+              //difference between margin and paddling
+              padding: EdgeInsetsGeometry.all(6),
+              margin: EdgeInsets.all(5),
+              color: Colors.amber,
+              child: const Text(
+                '1',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 187, 84, 76),
+                ),
               ),
             ),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                // label: Text(
-                //   'Enter the amount in USD',
-                //   style: TextStyle(color: Colors.red, fontSize: 20),
-                // ),
-                //we want some hint type thing which goes off when we write somethingh
-                hintText: 'Please enter amount you want to convert in USD',
-                hintStyle: TextStyle(color: Color.fromARGB(255, 230, 221, 221)),
-                //suffix can be used when we required to put at last place
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Color.fromARGB(255, 230, 221, 221),
-                filled: true,
-                fillColor: Color.fromARGB(225, 100, 100, 100),
-                enabled: true,
-                focusedBorder: OutlineInputBorder()
+            //padding:- when want margin for textfield as it does not come under property of textfield double click->refractor->wrap with padding (text field)
+            //container:- big brother of padding , conatain mainy property
+            //Padding(
+            Container(
+              padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+              child: TextField(
+                keyboardType: const TextInputType.numberWithOptions(
+                  signed: false,
+                  decimal: true,
+                ),
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 20, 19, 19),
+                  fontSize: 18,
+                ),
+                decoration: InputDecoration(
+                  // label: Text(
+                  //   'Enter the amount in USD',
+                  //   style: TextStyle(color: Colors.red, fontSize: 20),
+                  // ),
+                  //we want some hint type thing which goes off when we write somethingh
+                  hintText: 'Please enter amount you want to convert in USD',
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 230, 221, 221),
+                  ),
+                  //suffix can be used when we required to put at last place
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: const Color.fromARGB(255, 230, 221, 221),
+                  filled: true,
+                  fillColor: const Color.fromARGB(225, 100, 100, 100),
+                  enabled: true,
+                  focusedBorder: border,
+                  //now if I want that textfield wil be same before click and after click we use focused border when write somethingh and use enable border before writing somethingh
+                  enabledBorder: border,
+                ),
               ),
+            ),
+            //button:- there tow type of button here one is usaual like Icon and other is text button
+            //1. Raised , 2.Appears like a text
+            TextButton(
+              onPressed: () {
+                //there are three mode in app 1. debug :- when we are checking or creating
+                //2.release , 3. profile
+                if (kDebugMode) {
+                  print('clicked');
+                }
+              },
+              style: const ButtonStyle(
+                //But why MaterialStatePropertyAll is not working here ????
+                backgroundColor: WidgetStatePropertyAll(
+                  Color.fromARGB(255, 14, 223, 80),
+                ),
+                foregroundColor: WidgetStatePropertyAll(Colors.red),
+                fixedSize: WidgetStatePropertyAll(Size(300, 30)),
+              ),
+              child: const Text('Convert'),
             ),
           ],
         ),
