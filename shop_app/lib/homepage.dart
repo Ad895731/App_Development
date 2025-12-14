1dbc8d7c925a1704d28ba93c8ce8675b1706379f
aditya <adityan7800@gmail.com>
2025-12-14 10:06:58 +0530

    google_font,registerfont,Theme,safeareawidget,expand_textfield

--- shop_app/lib/homepage.dart
+++ shop_app/lib/homepage.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping App'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Shopping App!'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Shoes\nCollection',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // SizedBox(width: 20),
+                Expanded(
+                  child: TextField(
+                    controller: TextEditingController(),
+                    decoration: InputDecoration(
+                      //Icon is widget here and Icons is class here
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search',
+                      border: OutlineInputBorder(
+                        // borderRadius: BorderRadius.all(
+                        //   Radius.elliptical(50, 50),
+
+                        // topLeft: Radius.circular(30),
+                        //bottomLeft: Radius.circular(30),
+                        //topRight: Radius.circular(30),
+                        //bottomRight: Radius.circular(30),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(225, 225, 225, 1),
+                        ),
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(30),
+                          //right: Radius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/homepage.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Shoping App', home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+
+      //theme is used to set the global font family for the app
+      //colorScheme is used to set the primary color of the app , primary means ??
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(254, 206, 1, 1),
+        ),
+        //can be overriden by usual value 
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
+      ),
+      home: HomePage(),
+    );
   }
 }
